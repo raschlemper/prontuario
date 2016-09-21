81251256dc34201d0c9522e4e16bd51d4a2abd80
Raschlemper <raschlemper@gmail.com>
2016-09-21 07:14:03 -0300

    init

---
+++ client/new/js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var app = angular.module('Prontuario', [
+  'ngSanitize',
+  'ngCookies',
+  'ngResource',
+  'ngStorage',
+  'ngAnimate',
+  'ui.router',
+  'ui.bootstrap',
+  'pascalprecht.translate',
+  'ui.mask',
+  'ui.utils.masks',
+  'xeditable',
+  'appTable'
+]);
---
+++ client/new/js/run/xeditable.js
@@ -0,0 +1,7 @@
+'use strict'
+
+app.run(function(editableOptions) {
+
+  	editableOptions.theme = 'bs3';
+  	
+});
