b551580fbb5d17458b83a78861234ab62ed870db
rafael.schlemper <rafael.schlemper@gennera.com.br>
2016-12-07 15:48:14 -0200

    ajustes

--- client/js/controller/authController.js
+++ client/js/controller/authController.js
@@ -11,7 +11,7 @@ app.controller('AuthController', ['$rootScope', '$scope', '$state', '$stateParam
 	};
 
 	var getUrl = function(strategy) {
-		var url  = 'https://ras-administration.herokuapp.com/auth/login/';
+		var url  = 'https://prosoft-admin.herokuapp.com/auth/login/';
 		    url += strategy + '/';
 		    url += AuthService.getSystem();
 		    url += '?target=' + getTarget();
