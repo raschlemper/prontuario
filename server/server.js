f767992c5c602877436e3319f18f32934b3370db
Raschlemper <raschlemper@gmail.com>
2016-12-07 22:48:12 -0200

    ajustes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 var config = require('./config/environment');
 
 // Connect to database
+console.log(config.mongo.uri);
 mongoose.connect(config.mongo.uri, config.mongo.options);
 
 /**
